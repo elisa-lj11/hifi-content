0177886ca51bf93ac5f62125931d7167b5a6bed2
Elisa Lupin-Jimenez <elisa@highfidelity.io>
2018-05-14 13:36:24 -0700

    started retrieval server script for pinata

---
+++ DomainContent/Mexico/retrievePinataAndPaddle.js
@@ -0,0 +1,19 @@
+//
+// retrievePinataAndPaddle.js
+// A script to return the pinata and bat to their original locations
+// 
+// Author: Elisa Lupin-Jimenez
+// Copyright High Fidelity 2018
+//
+// Licensed under the Apache 2.0 License
+// See accompanying license file or http://apache.org/
+//
+// All assets are under CC Attribution Non-Commerical
+// http://creativecommons.org/licenses/
+//
+
+(function() {
+
+	
+
+});
