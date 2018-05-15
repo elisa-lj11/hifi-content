f4ba0e17542d3d9d96c0cc614459417c7c7dbba0
Elisa Lupin-Jimenez <elisa@highfidelity.io>
2018-05-15 09:19:33 -0700

    created entity script for pinata zone to check user entry

---
+++ DomainContent/Mexico/isPinataZoneEmpty.js
@@ -0,0 +1,20 @@
+//
+// isPinataZoneEmpty.js
+// A script to check when no users are in the pinata zone in order to
+// reset the pinata and pinata paddle
+// 
+// Author: Elisa Lupin-Jimenez
+// Copyright High Fidelity 2018
+//
+// Licensed under the Apache 2.0 License
+// See accompanying license file or http://apache.org/
+//
+// All assets are under CC Attribution Non-Commerical
+// http://creativecommons.org/licenses/
+//
+
+(function() {
+
+    
+
+});
